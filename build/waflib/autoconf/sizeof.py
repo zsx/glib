b12e7e0f650ae02552e3469feece459a0c1ed1fd
Shixin Zeng <zeng.shixin@gmail.com>
2010-08-04 01:03:21 +0200

    WIP: refactor

---
+++ build/waflib/autoconf/sizeof.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from waflib.Configure import conf, ConfigurationError
+from defaults import INCLUDES_DEFAULT
+
+COMPUTE_INT_CODE='''
+int
+main ()
+{
+	static int test_array [1 - 2 * !(%s)];
+	test_array [0] = 0 ;
+	return 0;
+}
+'''
+
+@conf
+def check_sizeof(self, t, lo = 1, hi=17, **kw):
+	define_name = 'SIZEOF_' + t.upper().replace(' ', '_').replace('*', 'P')
+	if self.options.cross_compile:
+		self.compute_sizeof(t, lo, hi, **kw)
+	else:
+		kw.update({'fragment': kw.get('headers', INCLUDES_DEFAULT) + 'int main() {printf("%%d", sizeof(%s));return 0;}' % t, 
+			   'execute':True, 
+                           'errmsg':'Unknown', 
+                           'define_name':define_name, 
+                           'define_ret':True, 
+                           'quote':False})
+		self.validate_c(kw)
+		self.start_msg('Checking for sizeof ' + t)
+		ret = None
+		try:
+			ret = self.run_c_code(**kw)
+		except:
+			self.undefine(define_name)
+			self.end_msg('Unknown', 'YELLOW')
+			raise
+			if 'mandatory' not in kw or kw['mandatory']:
+				self.fatal("can't determine size of " + t) 
+		else:
+			kw['success'] = ret
+			self.end_msg(ret)
+			self.post_check(**kw)
+
+@conf
+def compute_sizeof(self, t, lo=1, hi=17, **kw):
+	def try_to_compile(kw):
+		#self.check_cc(**kw)
+		#print ('kw=\n%s' %  kw)
+		self.validate_c(kw)
+		#print ('kw=\n%s' %  kw)
+		self.run_c_code(**kw)
+
+	self.start_msg('Checking for sizeof ' + t)
+	define_name = 'SIZEOF_' + t.upper().replace(' ', '_').replace('*', 'P')
+	if lo > hi:
+		self.undefine(define_name)
+		self.end_msg('Unknown', 'YELLOW')
+		self.fatal('lo(%d) must be smaller than hi(%d)' % (lo, hi))
+	while lo < hi:
+		cur = (lo + hi)//2
+		#print ('lo = %d, hi = %d, cur = %d' % (lo, hi, cur))
+		if cur == lo:
+			break
+		try:
+			# The cast to long int works around a bug in the HP C Compiler
+			# version HP92453-01 B.11.11.23709.GP, which incorrectly rejects
+			# declarations like `int a3[[(sizeof (unsigned char)) >= 0]];'.
+			# This bug is HP SR number 8606223364.
+			#print ('compiling (%s)' %  cur)
+			kw.update({'fragment': COMPUTE_INT_CODE % ('(long int) sizeof(%s) >= %s' % (t, cur))})
+			try_to_compile(kw)
+		except:
+			#print ('cur(%d) is too high, set it as high' %  cur)
+			hi = cur
+			cur = (lo + hi)//2
+		else:
+			#print ('cur(%d) is too low, set it as lo' %  cur)
+			lo = cur
+	else:
+		if lo > hi:
+			self.undefine(define_name)
+			self.end_msg('Unknown', 'YELLOW')
+			self.fatal("Unexpected: lo (%d) > hi (%d)" % (lo, hi)) 
+	#sizeof(t) should be 'lo', if it succeeded
+	#try one more time to make sure
+	try:
+		kw.update({'fragment': kw.get('headers', INCLUDES_DEFAULT) + COMPUTE_INT_CODE % ('(long int) sizeof(%s) == %s' % (t, lo))})
+		try_to_compile(kw)
+	except:
+		self.undefine(define_name)
+		self.end_msg('Unknown', 'YELLOW')
+		if 'mandatory' not in kw or kw['mandatory']:
+			self.fatal("can't compute size of " + t) 
+	else:
+		self.define(define_name, lo)
+		self.end_msg(str(lo))
---
+++ build/waflib/misc.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from waflib.Context import STDOUT, STDERR
+from waflib.Configure import conf, ConfigurationError
+from waflib.TaskGen import feature, before
+from waflib import Utils
+
+GNU_2_1_CODE='''
+#include <features.h>
+#ifdef __GNU_LIBRARY__
+ #if (__GLIBC__ == 2 && __GLIBC_MINOR__ >= 1) || (__GLIBC__ > 2)
+  #error "Not glibc 2.1"
+ #endif
+#else
+  #error "Not a glibc library"
+#endif
+
+int main()
+{
+	return 0;
+}
+'''
+
+@conf
+def check_libiconv(self, iconv):
+	if iconv == 'maybe':
+		found_iconv = False
+		if self.check_cc(function_name='iconv_open', header_name='iconv.h', msg = 'checking for iconv_open in C library', mandatory=False, uselib_store='ICONV'):
+			found_iconv = True
+		elif self.check_cc(function_name='libiconv_open', header_name='iconv.h', lib='iconv', msg='checking for libiconv_open in GNU libiconv', mandatory=False, defines=['USE_LIBICONV_GNU'], uselib_store='ICONV'):
+			found_iconv = True
+		elif self.check_cc(function_name='iconv_open', header_name='iconv.h', lib='iconv', msg='checking for iconv_open in the system library iconv', mandatory=False, defines='USE_LIBICONV_NATIVE', uselib_store='ICONV'):
+			found_iconv = True
+		if not found_iconv:
+			self.fatal('No iconv() implementation found in C library or libiconv')
+	elif iconv == 'no':
+		self.check_cc(function_name='iconv_open', header_name='iconv.h', msg = 'checking for iconv_open in C library', uselib_store='ICONV')
+	elif iconv in ('yes', 'gnu'):
+		self.check_cc(function_name='libiconv_open', header_name='iconv.h', lib='iconv', msg='checking for libiconv_open in GNU libiconv', defines=['USE_LIBICONV_GNU'], uselib_store='ICONV')
+	elif iconv == 'native':
+		self.check_cc(function_name='iconv_open', header_name='iconv.h', lib='iconv', msg='checking for iconv_open in the system library iconv', defines='USE_LIBICONV_NATIVE', uselib_store='ICONV')
+	else:
+		self.fatal('Unknown parameter to --with-libiconv')
+
+@conf
+def is_gnu_library_2_1(self):
+	try:
+		self.check_cc(fragment=GNU_2_1_CODE, msg="checking whether glibc 2.1", errmsg='No')
+	except ConfigurationError:
+		return False
+	else:	
+		return True
