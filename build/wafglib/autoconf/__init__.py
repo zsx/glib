875f1db164be01944646fa91f3588faad4d3e371
Shixin Zeng <zeng.shixin@gmail.com>
2010-08-17 07:45:55 +0200

    WIP: add vsnprintf_c99 checking

--- build/wafglib/autoconf/__init__.py
+++ build/wafglib/autoconf/__init__.py
@@ -6,5 +6,7 @@ from .compute_int import *
 from .sizeof import *
 from .stdc_headers import *
 from .alloca import *
+from .vsnprintf_c99 import *
+from .printf_unix98 import *
 
 #__all__ = ['autoconf', 'sizeof', 'stdc_headers', 'alloca']
---
+++ build/wafglib/autoconf/vsnprintf_c99.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from waflib.Errors import ConfigurationError
+from waflib.Configure import conf
+
+__all__ = ['check_vsnprintf_c99']
+
+VSNPRINTF_C99_CODE='''
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int
+doit(char * s, ...)
+{
+  char buffer[32];
+  va_list args;
+  int r;
+
+  va_start(args, s);
+  r = vsnprintf(buffer, 5, s, args);
+  va_end(args);
+
+  if (r != 7)
+    return 1;
+
+  /* AIX 5.1 and Solaris seems to have a half-baked vsnprintf()
+     implementation. The above will return 7 but if you replace
+     the size of the buffer with 0, it borks! */
+  va_start(args, s);
+  r = vsnprintf(buffer, 0, s, args);
+  va_end(args);
+
+  if (r != 7)
+    return 1;
+
+  return 0;
+}
+
+int
+main(void)
+{
+  return doit("1234567");
+}
+'''
+
+@conf
+def check_vsnprintf_c99(self, **kw):
+	if getattr(self.env, 'cross_compile', False):
+		raise self.fatal('cross_compiling, no vsnprintf_c99')
+	kw.update({'fragment':VSNPRINTF_C99_CODE, 'msg':'checking for C99 vsnprintf', 'execute':True, 'define_name':'HAVE_C99_VSNPRINTF'})
+	try:
+		self.check_cc(**kw)
+	except ConfigurationError:
+		#self.undefine('HAVE_C99_VSNPRINTF')
+		raise
