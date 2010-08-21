6a1c068b242a74b608c27942d5e9eb56ba7ef0b9
Shixin Zeng <zeng.shixin@gmail.com>
2010-08-21 07:16:15 +0200

    avoid compiling warnings

--- build/wafglib/autoconf/printf_unix98.py
+++ build/wafglib/autoconf/printf_unix98.py
@@ -7,6 +7,7 @@ from waflib.Configure import conf
 __all__ = ['check_printf_unix98']
 
 PRINTF_UNIX98_CODE='''
+#include <stdlib.h>
 #include <stdio.h>
 
 int
@@ -14,7 +15,7 @@ main (void)
 {
   char buffer[128];
 
-  sprintf (buffer, "%2\$d %3\$d %1\$d", 1, 2, 3);
+  sprintf (buffer, "%2$d %3$d %1$d", 1, 2, 3);
   if (strcmp ("2 3 1", buffer) == 0)
     exit (0);
   exit (1);
