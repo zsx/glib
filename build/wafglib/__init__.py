26f663320293b0080152b94bcbda7d38dffc278b
Shixin Zeng <zeng.shixin@gmail.com>
2010-08-28 01:12:37 +0200

    for the external waflib

---
+++ build/wafglib/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from lib.autoconf.alloca import *
+from lib.autoconf.misc import *
+from lib.autoconf.compute_int import *
+from lib.autoconf.endian import *
+from lib.autoconf.printf_unix98 import *
+from lib.autoconf.sizeof import *
+from lib.autoconf.stdc_headers import *
+from lib.autoconf.vsnprintf_c99 import *
+from lib.misc import *
+from lib.platinfo import PlatInfo
